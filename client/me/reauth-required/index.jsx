02bb40b59e7a5311041211697177250dec76102a
torres126 <torres126@clubpenguinmountains.com>
2019-04-06 00:23:42 +0100

    Update index.jsx

--- client/me/reauth-required/index.jsx
+++ client/me/reauth-required/index.jsx
@@ -195,7 +195,7 @@ const ReauthRequired = createReactClass( {
 			} );
 		
 		const showVerification = classNames( {
-					'is-visible': smsCodeSent;
+					'is-visible': smsCodeSent,
 			} );
 
 		return (
