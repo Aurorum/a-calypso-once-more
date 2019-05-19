8207f5c777dda545859d351e8b2028edc775ac09
torres126 <torres126@clubpenguinmountains.com>
2019-05-19 09:54:56 +0100

    Create index.js

---
+++ client/my-sites/exporter/index.js
@@ -0,0 +1,24 @@
+/**
+ * External dependencies
+ */
+import page from 'page';
+
+/**
+ * Internal dependencies
+ */
+import { makeLayout, render as clientRender } from 'controller';
+import { navigation, siteSelection, sites } from 'my-sites/controller';
+import { exportController } from './controller';
+
+export default () => {
+  page( '/exporter', siteSelection, sites, makeLayout, clientRender );
+  
+	page(
+		'/exporter/:site_id?',
+		siteSelection,
+		navigation,
+		helloWorld,
+		makeLayout,
+		clientRender
+	);
+}
