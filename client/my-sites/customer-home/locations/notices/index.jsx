f1d493afeb40e15c7f47cb41dbd420ec28b72453
Aurorum <torres126@clubpenguinmountains.com>
2020-04-09 23:02:13 +0100

    Update index.jsx

--- client/my-sites/customer-home/locations/notices/index.jsx
+++ client/my-sites/customer-home/locations/notices/index.jsx
@@ -1,38 +1,78 @@
 /**
  * External dependencies
  */
-import React from 'react';
-
+import React, { Component } from 'react';
+import { localize } from 'i18n-calypso';
+import { connect } from 'react-redux';
 /**
  * Internal dependencies
  */
-import CelebrateSiteCreation from 'my-sites/customer-home/cards/notices/celebrate-site-creation';
-import CelebrateSiteLaunch from 'my-sites/customer-home/cards/notices/celebrate-site-launch';
-import CelebrateSiteMigration from 'my-sites/customer-home/cards/notices/celebrate-site-migration';
-import CelebrateSiteSetupComplete from 'my-sites/customer-home/cards/notices/celebrate-site-setup-complete';
+import ActionPanel from 'components/action-panel';
+import ActionPanelTitle from 'components/action-panel/title';
+import ActionPanelBody from 'components/action-panel/body';
+import ActionPanelFigure from 'components/action-panel/figure';
+import ActionPanelCta from 'components/action-panel/cta';
+import ActionPanelFooter from 'components/action-panel/footer';
+import Gridicon from 'components/gridicon';
+import { Button } from '@automattic/components';
+import { getSelectedSiteId } from 'state/ui/selectors';
+import { savePreference } from 'state/preferences/actions';
 
-const cardComponents = {
-	'home-notice-celebrate-site-creation': CelebrateSiteCreation,
-	'home-notice-celebrate-site-launch': CelebrateSiteLaunch,
-	'home-notice-celebrate-site-migration': CelebrateSiteMigration,
-	'home-notice-celebrate-site-setup-complete': CelebrateSiteSetupComplete,
-};
+class RecommendedTask extends Component {		
+	render() {
+		const { translate } = this.props;
+		
+		let taskName;
+		let taskId;
+		let taskImage;
+		let taskDescription;
+		let taskMinutes;
+		let actionText;
+		let actionLink;
+		
+		if ( 2 > 1 ) {
+			taskName = translate( 'Connect social media accounts' );
+			taskId = 'connect-social-media';
+			taskImage = "/calypso/images/stats/tasks/social-links.svg";
+			taskDescription = translate( 'Integrate your site with social media to automatically post your content and drive content to your site.' );
+			taskMinutes = '5';
+			actionText = translate( 'Connect accounts' );
+		}
+		
+		const minuteOrMinutes = taskMinutes > 1 ? translate( 'minutes' ) : translate( 'minute' );
+		
+		return (
+            <ActionPanel>
+                <ActionPanelBody>
+                    <ActionPanelFigure align="right">
+                        <img
+                            src={ taskImage }
+                            alt=""
+                        />
+                    </ActionPanelFigure>
+					<div>
+						<Gridicon icon="time" />
+						<p>{ taskMinutes + ' ' + minuteOrMinutes }
+						</p>
+					</div>
+                    <ActionPanelTitle>{ taskName }</ActionPanelTitle>
+                    <p>{ taskDescription }</p>
+                    <ActionPanelCta>
+                        <Button className="action-panel__support-button" primary href={ actionLink }>
+							{ actionText }
+                        </Button>
+                        <Button className="action-panel__support-button is-link" href="/help/contact">
+							{ translate( 'Skip for now' ) }
+                        </Button>
+                    </ActionPanelCta>
+                </ActionPanelBody>
+            </ActionPanel>
+		);
+	}
+}
 
-const Notices = ( { checklistMode, displayChecklist, cards } ) => {
-	return (
-		<>
-			{ cards &&
-				cards.map(
-					( card, index ) =>
-						cardComponents[ card ] &&
-						React.createElement( cardComponents[ card ], {
-							key: index,
-							checklistMode,
-							displayChecklist,
-						} )
-				) }
-		</>
-	);
-};
-
-export default Notices;
+export default connect( state => {
+	return {
+		previousRoute: 2 > 1,
+	};
+} )( localize( RecommendedTask ) );
